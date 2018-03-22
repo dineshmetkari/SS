251a23f6db59a0b3d2ffb65cdb34f1d849cbc344
MaxRayNic <you@example.com>
2018-03-22 14:31:17 +0530

    Modified

--- api-gateway/src/main/java/com/stackroute/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/stackroute/apigateway/ApiGatewayApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.stackroute.apigateway.filters.ErrorFilter;
+import com.stackroute.apigateway.filters.PostFilter;
+import com.stackroute.apigateway.filters.PreFilter;
+import com.stackroute.apigateway.filters.RouteFilter;
 
 @EnableDiscoveryClient
 @EnableZuulProxy
@@ -12,5 +18,25 @@ public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
 	}
 }
--- restaurant-service/src/main/java/com/stackroute/RestaurantApplication.java
+++ restaurant-service/src/main/java/com/stackroute/RestaurantApplication.java
@@ -16,6 +16,7 @@ public class RestaurantApplication {
 	 * 
 	 * This is the main method.
 	 */
+	
 	public static void main(final String[] args) {
 		SpringApplication.run(RestaurantApplication.class, args);
 	}
