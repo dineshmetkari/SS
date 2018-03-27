c5ab7d6d0c47efb16f6b1d1c5f824e51e8094851
Yaash21 <yaashjain21212@gmail.com>
2018-03-27 11:28:36 +0530

    added ImageCrawlerService

---
+++ ImageCrawlerService/src/test/java/com/stackroute/messaging/ReceiverTest.java
@@ -0,0 +1,16 @@
+//package com.stackroute.messaging;
+//
+//import static org.junit.Assert.*;
+//
+//import org.junit.Test;
+//
+//public class ReceiverTest {
+//	
+//
+//
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+//
+//}
