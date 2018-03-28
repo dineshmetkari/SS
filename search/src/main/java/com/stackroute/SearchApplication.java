050d511817d2641a73284916c43f72717f890aea
anandsummer28 <anandsummer111@gmail.com>
2018-03-28 14:42:20 +0530

    SearchService add

---
+++ search/src/main/java/com/stackroute/SearchApplication.java
@@ -0,0 +1,17 @@
+package com.stackroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+/**
+ * Main application 
+ * @author summer
+ *
+ */
+public class SearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchApplication.class, args);
+	}
+}
