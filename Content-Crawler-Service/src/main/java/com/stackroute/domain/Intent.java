9bff32d0b769106b3f51bd7bc2b567a8c30d9d2b
Garima Chaudhary <garima.skit@gmail.com>
2018-03-28 15:15:18 +0530

    Adding content crawler service

---
+++ Content-Crawler-Service/src/main/java/com/stackroute/domain/Intent.java
@@ -0,0 +1,51 @@
+package com.stackroute.domain;
+
+import java.util.List;
+
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+
+@NodeEntity
+public class Intent {
+	
+	@GraphId
+	private int id;
+	private String name;
+	
+	@Relationship(type="Is_A", direction = Relationship.INCOMING)	 
+    private List<IsARelation> indicators;
+	
+	
+	@Relationship(type="Is_An")
+	private Root root;
+	
+	public Root getRoot() {
+		return root;
+	}
+	public void setRoot(Root root) {
+		this.root = root;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<IsARelation> getIndicators() {
+		return indicators;
+	}
+	public void setIndicators(List<IsARelation> indicators) {
+		this.indicators = indicators;
+	}
+	
+
+}
