c3cce2da8f1d12e253b6375dc60e754105df1f01
vaishnavi.nakka <vaishnavi.nakka@cgi.com>
2018-05-03 13:10:10 +0530

    added neo4j data and code-service

--- Code-Crawler-Service/src/main/java/com/stackroute/CodeCrawlerServiceApplication.java
+++ Code-Crawler-Service/src/main/java/com/stackroute/CodeCrawlerServiceApplication.java
@@ -12,6 +12,7 @@ import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
@@ -25,13 +26,13 @@ import com.stackroute.listener.MessageListener;
  *
  */
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class CodeCrawlerServiceApplication {
 	/**
 	 * Name of the exchange
 	 */
 	
-	@Value("${topicexchange}")
+	
 	public String topicExchange="integration.exchange";
 	
 	/**
